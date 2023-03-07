4ec79eee9f9a5aab74f5347364244be33f6aaf4f
diiev <ramzans096@gmail.com>
2023-03-08 00:02:28 +0300

    верста страницы каталог

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -70,6 +70,7 @@ var stocks__cardslider = tns({
   loop: false,
   controls: false,
   nav: true,
+  mouseDrag: true,
   navPosition: "bottom",
   speed: "800"
  
@@ -90,3 +91,35 @@ document.querySelector('.ideas__slider_prev').addEventListener('click',function 
 document.querySelector('.ideas__slider_next').addEventListener('click',function () {
   ideas__slider.goTo('next');
 });
+
+const btnUp = {
+  el: document.querySelector('.scrollTop'),
+  show() {
+    // удалим у кнопки класс scrollTop_hide
+    this.el.classList.remove('scrollTop_hide');
+  },
+  hide() {
+    // добавим к кнопке класс scrollTop_hide
+    this.el.classList.add('scrollTop_hide');
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener('scroll', () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      // если страница прокручена больше чем на 900px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 900 ? this.show() : this.hide();
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector('.scrollTop').onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+
+btnUp.addEventListener();
